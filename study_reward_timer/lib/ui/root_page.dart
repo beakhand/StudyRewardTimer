aeebac5f7a49959b079bc84974cf00eebc650ac4
kamijo tomoo <cat51_2@hotmail.co.jp>
2021-07-09 01:09:23 +0900

    imple

--- study_reward_timer/lib/ui/root_page.dart
+++ study_reward_timer/lib/ui/root_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:study_reward_timer/ui/styles/app_colors.dart';
+import 'package:study_reward_timer/ui/styles/app_text_styles.dart';
 
 class RootPage extends HookWidget {
   @override
@@ -9,6 +11,38 @@ class RootPage extends HookWidget {
     //final controller = useProvider(rootPageProvider);
 
     return Scaffold(
+      appBar: AppBar(
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              child: Text("おべんきょうげーむたいまー",
+                  style: TextStyle(
+                      fontFamily: AppFontFamily.notoSansCJKjp,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: AppColors.appThemeColor()
+                  ),
+                ),
+              decoration: BoxDecoration(
+                color: Colors.red
+              ),
+            ),
+            ListTile(
+              title: Text("設定"),
+              onTap: () {
+              },
+            ),
+            ListTile(
+              title: Text("このアプリについて"),
+              onTap: () {
+              },
+            ),
+          ],
+        ),
+
+      ),
       body: Container(
           child: Text("test"),
           color: Colors.red
---
+++ study_reward_timer/lib/ui/styles/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color appThemeColor() {
+    return Color.fromRGBO(5, 211, 208, 1);
+  }
+
+  //static const Color appThemeColorConst = Color(0xFFFFF0FF);
+
+  static Color appThemeColorOpacity() {
+    return Color.fromRGBO(5, 211, 208, 0.5);
+  }
+
+}
